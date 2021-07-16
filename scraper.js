59eb78f0222b742427343ec54308bf71c1a72b23
Joshua Lundregan <jlundregan3@gatech.edu>
2021-07-15 21:31:43 -0400

    Swapped out all references to IMDb with references to TMDB and its respective element selectors.

--- scraper.js
+++ scraper.js
@@ -4,8 +4,8 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 
-let searchUrl  = "https://www.imdb.com/find?&s=tt&ref_=nv_sr_sm&q=";
-let movieUrl = "https://www.imdb.com/title/";
+let searchUrl  = "https://www.themoviedb.org/search/movie?query=";
+let movieUrl = "https://www.themoviedb.org/movie/";
 
 const searchCache = {};
 const movieCache = {};
@@ -14,6 +14,8 @@ const movieCache = {};
 function searchMovies(searchTerm) {
   //console.log('This is the searched URL:' + searchUrl + searchTerm);
 
+  //This caches the results of the search, in case the user has already searched this.
+  //I might delete this later, this came from the coding garden tutorial
   if(searchCache[searchTerm]){
     console.log('Serving from cache: ', searchTerm);
     return Promise.resolve(searchCache[searchTerm]);
@@ -24,17 +26,16 @@ function searchMovies(searchTerm) {
     .then(body => {
       const movies = [];
       const $ = cheerio.load(body);
-      $('.findResult').each(function(i, element) {
+      $('.results div.card').each(function(i, element) {
         const $element = $(element);
-        const $image = $element.find('td a img');
-        const $title = $element.find('td.result_text a')
+        const $image = $element.find('.image .poster img');
+        const $title = $element.find('.details .title div a');
 
-        const imdbID = $title.attr('href').match(/title\/(.*)\//)[1];
+        //const imdbID = $title.attr('href').match(/title\/(.*)\//)[1];
 
         const movie = {
-          image: $image.attr('src'),
-          title: $title.text(),
-          imdbID
+          image: "https://www.themoviedb.org" + $image.attr('src'),
+          title: $title.text()
         };
         movies.push(movie);
       });
@@ -45,53 +46,66 @@ function searchMovies(searchTerm) {
     });
 }
 
-function getMovie(imdbID){
+function getMovie(tmdbID){
+  //console.log('This is the searched URL:' + movieUrl + tmdbID);
 
-  if(movieCache[imdbID]){
-    console.log('Serving from cache: ', imdbID);
-    return Promise.resolve(movieCache[imdbID]);
+  if(movieCache[tmdbID]){
+    console.log('Serving from cache: ', tmdbID);
+    return Promise.resolve(movieCache[tmdbID]);
   }
 
-  return fetch(`${movieUrl}${imdbID}`)
+  return fetch(`${movieUrl}${tmdbID}`)
     .then(response => response.text())
     .then(body => {
       const $ = cheerio.load(body);
-      const $title = $('.ipc-page-section div div h1');
+      const $title = $('.title h2 a');
       const title = $title.text();
 
+      //This was for the imdb scraper, from which this code was copied
       //Departing from the video
       //const metaInfo = $('.ipc-inline-list.baseAlt.ipc-inline-list--show-dividers li');
-      const metaInfo = $('ul[data-testid="hero-title-block__metadata"] li');
-      let metaInfoArray = []
-      metaInfo.each(function(i, elm) {
-        metaInfoArray.push($(this).text()); // for testing do text()
-      });
+      // const metaInfo = $('ul[data-testid="hero-title-block__metadata"] li');
+      // let metaInfoArray = []
+      // metaInfo.each(function(i, elm) {
+      //   metaInfoArray.push($(this).text()); // for testing do text()
+      // });
+      //
+      // const year = metaInfoArray[0].slice(0, metaInfoArray[0].length / 2);
+      // const rating = metaInfoArray[1].slice(0, metaInfoArray[1].length / 2);
+      // const runTime = metaInfoArray[2];
+      const $year = $(".title .release_date");
+      const year = $year.text().slice(1,-1);
 
-      const year = metaInfoArray[0].slice(0, metaInfoArray[0].length / 2);
-      const rating = metaInfoArray[1].slice(0, metaInfoArray[1].length / 2);
-      const runTime = metaInfoArray[2];
-      //console.log(metaInfoArray);
+      const $rating = $(".certification");
+      const rating = $rating.text();
 
-      const poster = $('img.ipc-image').attr('src');
-      const summary = $("span[data-testid='plot-xs_to_m']").text();
-      const director = $(".ipc-metadata-list-item__list-content-item--link").first().text();
-      const metascore = $('.score-meta').text();
+      const $runTime = $(".runtime");
+      const runTime = $runTime.text();
+
+      const poster = "https://www.themoviedb.org" + $('.poster_wrapper img.poster').attr('src').replace("_filter(blur)", "");
+      const summary = $(".overview p").text();
+
+      const crew = [];
+      $(".people li.profile a").each(function(i, element) {
+        const crewMember = $(element).text();
+        crew.push(crewMember);
+      })
+      const director = crew[0];
 
       const movie = {
-        imdbID,
+        tmdbID,
         title,
         year,
         rating,
         runTime,
         poster,
         summary,
-        director,
-        metascore
+        director
       };
       // console.log(body);
       // return {body}
 
-      movieCache[imdbID] = movie;
+      movieCache[tmdbID] = movie;
 
       return movie;
     });
