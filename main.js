8ef08eb164a49bce0b1f3f4138bffc675e1f7909
Joshua Lundregan <jlundregan3@gatech.edu>
2022-02-16 17:03:54 -0500

    Made installer with electron winstaller, adding approrpriate code to main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const { app, BrowserWindow } = require('electron');
-//var Datastore = require('nedb'), db = new Datastore({ filename: './Files/data.db', autoload: true });
+
+//This is here for the installer, copied from
+//https://ourcodeworld.com/articles/read/365/how-to-create-a-windows-installer-for-an-application-built-with-electron-framework
+if (handleSquirrelEvent(app)) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
 
 app.whenReady().then(() => {
   createWindow();
@@ -12,7 +18,67 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin'){
     app.quit();
   }
-})
+});
+
+//once again, this is all for squirrel stuff, copied from the link above
+const ChildProcess = require('child_process');
+const path = require('path');
+
+const appFolder = path.resolve(process.execPath, '..');
+const rootAtomFolder = path.resolve(appFolder, '..');
+const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+const exeName = path.basename(process.execPath);
+
+const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+        spawnedProcess = ChildProcess.spawn(command, args, {
+            detached: true
+        });
+    } catch (error) {}
+
+    return spawnedProcess;
+};
+
+const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+};
+
+const squirrelEvent = process.argv[1];
+switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+        // Optionally do things such as:
+        // - Add your .exe to the PATH
+        // - Write to the registry for things like file associations and
+        //   explorer context menus
+
+        // Install desktop and start menu shortcuts
+        spawnUpdate(['--createShortcut', exeName]);
+
+        setTimeout(application.quit, 1000);
+        return true;
+
+    case '--squirrel-uninstall':
+        // Undo anything you did in the --squirrel-install and
+        // --squirrel-updated handlers
+
+        // Remove desktop and start menu shortcuts
+        spawnUpdate(['--removeShortcut', exeName]);
+
+        setTimeout(application.quit, 1000);
+        return true;
+
+    case '--squirrel-obsolete':
+        // This is called on the outgoing version of your app before
+        // we update to the new version - it's the opposite of
+        // --squirrel-updated
+
+        application.quit();
+        return true;
+}
+};
 
 
 function createWindow () {
