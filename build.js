f6a9ea01e14c807888405ed17f55b99cc7c77bf4
Joshua Lundregan <jlundregan3@gatech.edu>
2022-02-16 18:59:43 -0500

    add build.js file

---
+++ build.js
@@ -0,0 +1,25 @@
+//This is copied from https://ourcodeworld.com/articles/read/365/how-to-create-a-windows-installer-for-an-application-built-with-electron-framework
+
+var electronInstaller = require('electron-winstaller');
+const path = require('path');
+
+// In this case, we can use relative paths
+var settings = {
+    // Specify the folder where the built app is located
+    appDirectory: './MovieShelf-win32-x64',
+    // Specify the existing folder where
+    outputDirectory: './MovieShelf-built-installers',
+    // The name of the Author of the app (the name of your company)
+    authors: 'Joshua Lundregan',
+    // The name of the executable of your built
+    exe: './MovieShelf.exe',
+    iconURL: 'MovieShelf/images/icon.ico'
+};
+
+resultPromise = electronInstaller.createWindowsInstaller(settings);
+
+resultPromise.then(() => {
+    console.log("The installers of your application were succesfully created !");
+}, (e) => {
+    console.log(`Well, sometimes you are not so lucky: ${e.message}`)
+});
